d734ea014e7e3424e00f14ae5c42fd12b632f249
gbrreel <gabrielggalloo@gmail.com>
2025-08-14 21:15:32 -0300

    Inicio do projeto

---
+++ src/test/java/com/news/news_ai/NewsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.news_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
